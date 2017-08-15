96d458b798a4160ab9a7b317722d0ee1d55ea030
Yutaka Nishimura <ytk.nishimura@gmail.com>
2017-08-15 23:57:57 +0900

    add Opts test

--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"net/http"
+	"net/url"
+
+	"io"
+
 	"github.com/olorin/nagiosplugin"
 )
 
-func Check() (chk *nagiosplugin.Check, err error){
+func Check() (chk *nagiosplugin.Check, err error) {
 	check := nagiosplugin.NewCheck()
 	defer check.Finish()
-	check.AddResult(nagiosplugin.OK, "good")
+
+	check.AddResult(nagiosplugin.CRITICAL, "critical")
 	return check, nil
 }
 
+type Opts struct {
+	Client *http.Client
+	Url    *url.URL
+	Method string
+	Body   io.Reader
+}
+
+var DefaultOpts = &Opts{
+	Client: http.DefaultClient,
+}
+
+var options *Opts
+
+func SetOpts(opts *Opts) {
+	options = opts
+}
 func main() {
 	Check()
 }
--- main_test.go
+++ main_test.go
@@ -1,17 +1,54 @@
 package main
 
 import (
+	"log"
 	"reflect"
 	"testing"
+
+	"net/http"
+
+	"net/url"
+
+	"strings"
 
 	"github.com/olorin/nagiosplugin"
 )
 
 var checkResult *nagiosplugin.Check
+var optsResult *Opts
 
 func init() {
 	checkResult = nagiosplugin.NewCheck()
 	checkResult.AddResult(nagiosplugin.OK, "good")
+
+	optsResult = DefaultOpts
+	u, err := url.Parse("http://localhost/data")
+	if err != nil {
+		log.Fatal(err)
+	}
+	optsResult.Url = u
+	optsResult.Method = "POST"
+	optsResult.Body = strings.NewReader(`{"message":"OK?"}`)
+
+	SetOpts(optsResult)
+}
+
+func TestOpts(t *testing.T) {
+
+	u, err := url.Parse("http://localhost/data")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	opts := &Opts{
+		Client: http.DefaultClient,
+		Url:    u,
+		Method: "POST",
+		Body:   strings.NewReader(`{"message":"OK?"}`),
+	}
+	if !reflect.DeepEqual(optsResult, opts) {
+		t.Fatalf("Didn't match Opts.\n want: %q,\n have: %q\n", optsResult, opts)
+	}
 }
 
 func TestCheck(t *testing.T) {
@@ -21,6 +58,6 @@ func TestCheck(t *testing.T) {
 		t.Error(err)
 	}
 	if !reflect.DeepEqual(checkResult, check) {
-		t.Fatalf("Didn't match check Result.\n want: %q,\n have: %q", checkResult, check)
+		t.Fatalf("Didn't match check Result.\n want: %q,\n have: %q\n", checkResult, check)
 	}
 }
